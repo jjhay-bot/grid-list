a85d110671e0fdbb15086ce0983463be95d3c2e7
Jhay Jhay Alcorcon <jhay.alcorcon@growsari.com>
2022-02-13 00:09:43 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/deploy-react-project-sample/',
+  base: 'https://jjhay-bot.github.io/grid-list',
   plugins: [react()]
 })
